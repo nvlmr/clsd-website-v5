34ac9366bb0490fba50fe5f9cf29d49aed2f5d52
Louis <147993282+nvlmr@users.noreply.github.com>
2026-02-24 12:39:43 +0800

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
 import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
