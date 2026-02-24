419266c6fe7d38ebacffed03354f394106b3053d
Louis <147993282+nvlmr@users.noreply.github.com>
2026-02-24 12:50:45 +0800

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import Home from "./pages/Home";
-import Landing from "./pages/Landing";
+import Home from "../pages/Home";
+import Landing from "../pages/Landing";
 
 function App() {
   return (
